8f30ed1aa6122dbd969fd8e113639931b9f1e3e0
productdevbook <38668796+productdevbook@users.noreply.github.com>
2021-11-15 10:05:14 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,23 +7,10 @@
 <script lang="ts">
 import { computed, defineComponent, ref } from 'vue'
 
-interface feature {
-  id: number
-  title: string
-  summary: string
-}
 export default defineComponent({
   setup() {
-    const featuresData = ref<feature[]>([
-      {
-        id: 1,
-        title: 'Book',
-        summary: 'Add to library book and book read status save huntersofbook.',
-      },
-      { id: 2, title: 'Shelf', summary: 'Create special shelves for your books.' },
-    ])
 
-    return { featuresData }
+    return {}
   },
   head: {},
 })
