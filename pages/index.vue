69f040c9dc2d810681e8be7a5cfcd727e048d094
productdevbook <38668796+productdevbook@users.noreply.github.com>
2021-11-29 15:40:32 +0300

    core: vite_ssr_import

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,54 @@
 <template>
-  <div>
-    <div>asdasd</div>
-    <AtomHInput v-model="form.email" />
+  <div class="grid grid-cols-5 min-h-screen background-ts">
+    <AtomHInput v-model="email" forname="email" type="email" />
   </div>
 </template>
 
 <script lang="ts">
-  import {defineComponent, ref} from 'vue'
-
+  import {defineComponent} from 'vue'
+  import {useField, useForm} from 'vee-validate'
+  import {string, object} from 'yup'
   export default defineComponent({
     setup() {
-      const form = ref({
-        email: '',
-        password: '',
+      const schema = object({
+        email: string().required().email(),
+        password: string().min(8).required(),
       })
-    
 
-      return {
+      const {errors, values, handleSubmit, meta, resetForm} =
+        useForm({
+          validationSchema: schema,
+          initialValues: {
+            email: '',
+            password: '',
+          },
+        })
 
-        form,
+      const {value: email, meta: emailMeta} = useField('email')
+      const {value: password, meta: passwordMeta} = useField('password')
+      const fieldReturn = {
+        email,
+        password,
+        emailMeta,
+        passwordMeta,
       }
-    },
+      
+      return {
+        ...fieldReturn,
+      }
+    }
   })
 </script>
+
+<style>
+  .scroll-bg {
+    -webkit-animation-name: scroll;
+    animation-name: scroll;
+    -webkit-animation-duration: 30s;
+    animation-duration: 30s;
+    -webkit-animation-timing-function: linear;
+    animation-timing-function: linear;
+    -webkit-animation-iteration-count: infinite;
+    animation-iteration-count: infinite;
+  }
+</style>
